4b74eff9115a1438a9e68193bd81ed0e4fa905c7
Andrew Ebert <andrew.j.ebert@gmail.com>
2016-05-20 12:44:54 -0500

    "Starting basic changes"

--- src/main/java/com/bigeebs/bigtransport/BigTransport.java
+++ src/main/java/com/bigeebs/bigtransport/BigTransport.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package com.bigeebs.bigtransport;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BigTransport.MODID, version = BigTransport.VERSION)
+public class BigTransport
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "bigtransport";
+    public static final String VERSION = "0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
